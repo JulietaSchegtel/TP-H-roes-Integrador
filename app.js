97d974d9c7ce050c67384996f0413c266820fb33
JuliSchegtel <julieta.schegtel@gmail.com>
2022-06-28 20:00:08 -0300

    Agrego rutas

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3030;
 
+
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
 app.get('/babbage', (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')))
 app.get('/berners-lee', (req, res) => res.sendFile(path.join(__dirname, 'views', 'berners-lee.html')))
