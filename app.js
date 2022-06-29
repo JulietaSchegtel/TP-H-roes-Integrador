2fcd316586c2a0676ec729145d085be2d2c7e099
JuliSchegtel <julieta.schegtel@gmail.com>
2022-06-28 21:23:30 -0300

    Modifico rutas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3030;
 
 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')))
 app.get('/babbage', (req, res) => res.sendFile(path.join(__dirname, 'views', 'babbage.html')))
@@ -14,15 +15,5 @@ app.get('/hopper', (req, res) => res.sendFile(path.join(__dirname, 'views', 'hop
 app.get('/lovelace', (req, res) => res.sendFile(path.join(__dirname, 'views', 'lovelace.html')))
 app.get('/turing', (req, res) => res.sendFile(path.join(__dirname, 'views', 'turing.html')))
 
-/*
-/ -> Debe direccionar al recurso index.html.
- /babbage -> Debe direccionar al recurso babbage.html
- /berners-lee -> Debe direccionar al recurso berners-lee.html.
- /clarke -> Debe direccionar al recurso clarke.html.
- /hamilton -> Debe direccionar al recurso hamilton.html.
- /hopper -> Debe direccionar al recurso hopper.html.
- /lovelace -> Debe direccionar al recurso lovelace.html.
- /turing-> Debe direccionar al recurso turing.html.
- */
 
 app.listen(port, () => console.log('Server running in http://localhost:' + port))
